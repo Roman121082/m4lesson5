7bc5e1a51d37af1a2a52be5c241d8d3cb3f08a77
rouman1201082 <chernovrn@gmail.com>
2024-02-04 18:51:45 +0400

    fixed prime number

--- task02.js
+++ task02.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const isPrime = (number) => {
-
-  for (let i = 2; i < number; i++) {
-    if (number % i !== 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  const squareRoot = Math.floor(Math.sqrt(number));
+  for (let i = 2; i <= squareRoot; i++) if (number % i === 0) return false;
+  return number >= 2;
 }
 
-console.log(`результат проверки на простое число: ${isPrime(911)}`);
+console.log(`результат проверки на простое число: ${isPrime(53)}`);
+
+for (let i = -100, count = 0; i < 100; i++) {
+  if (isPrime(i)) {
+    console.log(`${++count}) простое число ${i};`);
+  }
+}
